8c5436b0986ffd2abeebf6f46a3a8e78f99d5c71
Rahul91 <priyrahulmishra@gmail.com>
2015-04-29 01:14:33 +0530

    commtting

--- django_test/settings.py
+++ django_test/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['/home/rahul/python/django-rahul/django_test/templates/',
                   '/home/rahul/python/django-rahul/django_test/templates/pincode',
-                  os.path.join(PROJECT_DIR, "templates"),
+                  'app/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
